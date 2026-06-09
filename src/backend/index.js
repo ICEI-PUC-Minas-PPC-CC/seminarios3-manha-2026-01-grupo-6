990ed479207ae4d9a7cd11f1a3e126c0306f1f55
Domiciano Vieira <dgnvieira@sga.pucminas.br>
2026-06-09 16:26:51 -0300

    pontos finais adicionados ao significado das palavras

--- src/backend/index.js
+++ src/backend/index.js
@@ -13,27 +13,27 @@ const virtudesCardeais = [
         id: 0,
         titulo: "Virtude",
         youtubeId: "wFOD0K1xWtI", //Reservado para o id real do vídeo fornecido pela instituição
-        significado: "Virtude é a capacidade constante de escolher o que é correto e bom de forma consciente"
+        significado: "Virtude é a capacidade constante de escolher o que é correto e bom de forma consciente."
     },
     {
         id: 1,
         titulo: "Prudência",
         youtubeId: "AJd5Jh1t9YE", //Reservado para o id real do vídeo fornecido pela instituição
-        significado: "A virtude de agir com cautela e sensatez"
+        significado: "A virtude de agir com cautela e sensatez."
     },
 
     {
         id: 2,
         titulo: "Justiça",
         youtubeId: "BJqfHpAFXt0", //Reservado para o id real do vídeo fornecido pela instituição
-        significado: "Princípio ético e moral que busca a o equilíbrio e a igualdade de direitos"
+        significado: "Princípio ético e moral que busca a o equilíbrio e a igualdade de direitos."
     },
 
     {
         id: 3,
         titulo: "Fortaleza",
         youtubeId: "ZEAxQlJSPMY", //Reservado para o id real do vídeo fornecido pela instituição
-        significado: "A firmeza e constância na luta pelo bem, mesmo diante de obstáculos"
+        significado: "A firmeza e constância na luta pelo bem, mesmo diante de obstáculos."
     },
 
     {
